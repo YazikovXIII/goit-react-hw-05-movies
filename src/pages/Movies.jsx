24c63c3cf6c5aa7ee26911909ba3a8c03b352c85
Влад Язиков <vladzaraza@gmail.com>
2023-07-18 09:27:34 +0300

    123

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,12 +1,77 @@
+// import React, { useState, useEffect } from 'react';
+// import { Outlet, useSearchParams, useLocation } from 'react-router-dom';
+// import { fetchMoviesBySearchQuery } from 'helpers/fetchDataMovies';
+// import { StyledMoviesList } from './Home.styled';
+// import { NotStyledLink } from './Home.styled';
+// import { debounce } from 'lodash';
+
+// const Movies = () => {
+//   const [searchResults, setSearchResults] = useState([]);
+//   const location = useLocation();
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const searchQuery = searchParams.get('search') || '';
+
+//   useEffect(() => {
+//     const fetchMovies = async () => {
+//       try {
+//         const movies = await fetchMoviesBySearchQuery(searchQuery);
+//         setSearchResults(movies);
+//       } catch (error) {
+//         console.error('Oooops!', error.message);
+//       }
+//     };
+
+//     const delayedFetchMovies = debounce(fetchMovies, 500);
+
+//     delayedFetchMovies();
+
+//     return () => {
+//       delayedFetchMovies.cancel();
+//     };
+//   }, [searchQuery]);
+
+//   const handleSearchInputChange = event => {
+//     setSearchParams({ search: event.target.value });
+//   };
+
+//   return (
+//     <div>
+//       <h3>Search movies</h3>
+//       <form>
+//         <input
+//           type="text"
+//           value={searchQuery}
+//           onChange={handleSearchInputChange}
+//         />
+//       </form>
+//       <Outlet />
+//       <StyledMoviesList>
+//         {searchResults.map(movie => (
+//           <li key={movie.id}>
+//             <NotStyledLink
+//               to={`/movies/${movie.id}`}
+//               state={{ from: location }}
+//             >
+//               {movie.title}
+//             </NotStyledLink>
+//           </li>
+//         ))}
+//       </StyledMoviesList>
+//     </div>
+//   );
+// };
+
+// export default Movies;
+
 import React, { useState, useEffect } from 'react';
 import { Outlet, useSearchParams, useLocation } from 'react-router-dom';
 import { fetchMoviesBySearchQuery } from 'helpers/fetchDataMovies';
 import { StyledMoviesList } from './Home.styled';
 import { NotStyledLink } from './Home.styled';
-import { debounce } from 'lodash';
 
 const Movies = () => {
   const [searchResults, setSearchResults] = useState([]);
+  const [inputValue, setInputValue] = useState('');
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const searchQuery = searchParams.get('search') || '';
@@ -21,28 +86,30 @@ const Movies = () => {
       }
     };
 
-    const delayedFetchMovies = debounce(fetchMovies, 500);
-
-    delayedFetchMovies();
-
-    return () => {
-      delayedFetchMovies.cancel();
-    };
+    if (searchQuery) {
+      fetchMovies();
+    }
   }, [searchQuery]);
 
   const handleSearchInputChange = event => {
-    setSearchParams({ search: event.target.value });
+    setInputValue(event.target.value);
+  };
+
+  const handleSearchSubmit = event => {
+    event.preventDefault();
+    setSearchParams({ search: inputValue });
   };
 
   return (
     <div>
       <h3>Search movies</h3>
-      <form>
+      <form onSubmit={handleSearchSubmit}>
         <input
           type="text"
-          value={searchQuery}
+          value={inputValue}
           onChange={handleSearchInputChange}
         />
+        <button type="submit">Search</button>
       </form>
       <Outlet />
       <StyledMoviesList>
